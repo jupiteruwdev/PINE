f65f4f9fc8f58985cc2614c8662865db02c184fb
alex-at-pine <96759630+alex-at-pine@users.noreply.github.com>
2022-01-02 22:37:09 -0500

    total borrowed in usd

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import Web3 from "web3"
 import config from "./vars.conf"
-import { getCryptoPrice } from "./helper"
+import { getAllV1LoanEvents, getCryptoPrice } from "./helper"
 const app = express()
 app.use(cors())
 app.get( "/eth-balance", async ( req, res ) => {
@@ -18,15 +18,16 @@ app.get( "/global-stats", async ( req, res ) => {
   const sumOfArray = (y:Array<number>)=>y.reduce((previousValue: number, currentValue: number) => previousValue + currentValue)
   const web3 = new Web3((config.ethRPC as any)[1])
   const ethTVL = await Promise.all(config.v1pools.map(async e => (await web3.eth.getBalance(e)) |> web3.utils.fromWei |> Number)) |> sumOfArray
+  const totalBorrowed = await Promise.all(config.v1pools.map(e => getAllV1LoanEvents(web3, e))) |> (y => y.flatMap(e => e.map(x => (x as any).returnValues.loan[4] |> web3.utils.fromWei |> Number))) |> sumOfArray
   res.json( {
     usd_tvl: ethTVL * ethPrice.price,
+    usd_total_borrowed: totalBorrowed * ethPrice.price,
     block_number: await web3.eth.getBlockNumber()
   })
 } );
 
 // start the Express server
 (async () => {
-
     app.listen( 8080 )
     console.log('started')
 })()
