758a72a279e95750d1f5c55d59f0e275bc5416f1
Andrew Wei <andrewscwei@gmail.com>
2022-02-07 15:06:04 -0800

    Add logger

--- src/routes/pools/getEthLoanPool.ts
+++ src/routes/pools/getEthLoanPool.ts
@@ -5,17 +5,22 @@ import getPoolCapacity from '../../core/getPoolCapacity'
 import getPoolCollaterals from '../../core/getPoolCollaterals'
 import LoanPool from '../../entities/LoanPool'
 import { EthNetwork, getEthBlockNumber } from '../../utils/ethereum'
+import logger from '../../utils/logger'
 
 export default function getEthLoanPool(): RequestHandler {
   return async (req, res) => {
     const networkId = _.toNumber(req.query.network_id ?? EthNetwork.MAIN)
     const poolAddress = req.params.address
 
+    logger.info(`Fetching ETH loan pool for address <${poolAddress}> on network <${networkId}>...`)
+
     const nftIds = await getPoolCollaterals(poolAddress, { networkId })
     const utilizationEth = _.sum(await Promise.all(nftIds.map(nftId => getCollateralOutstanding(nftId, poolAddress, { networkId }))))
     const balanceEth = await getPoolCapacity(poolAddress, { networkId })
     const tvlEth = balanceEth + utilizationEth
     const blockNumber = await getEthBlockNumber()
+
+    logger.info(`Fetching ETH loan pool for address <${poolAddress}> on network <${networkId}>... OK`)
 
     const payload: LoanPool = {
       'eth_tvl': tvlEth,
