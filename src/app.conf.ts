f8ac5a25ca1b0ce4b8c55cbad6851074433241d6
Andrew Wei <andrewscwei@gmail.com>
2022-02-08 23:59:44 -0800

    Remove unused variables in app config

--- src/app.conf.ts
+++ src/app.conf.ts
@@ -1,6 +1,5 @@
 export default {
   port: process.env.PORT ?? 8080,
-  ethPoolAddress: process.env.ETH_POOL || '0xBF439a8337B9805Ca0BFCCaCEc08E3B0292F5143',
   openseaAPIKey: process.env.OPENSEA_API_KEY,
   ethRPC: {
     4: process.env.DEV_ETH_RPC || 'https://rinkeby.infura.io/v3/fad40c6991a64c0db19de9420e2ace3f',
