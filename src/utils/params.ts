001fb7f63bb810fa76745ecc78054898de83628d
Andrew Wei <andrewscwei@gmail.com>
2022-02-11 10:39:11 -0800

    Fix build

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -1,6 +1,6 @@
-import _ from 'lodash';
-import { BlockchainDict, EthBlockchain } from '../entities/Blockchain';
-import { EthNetwork, parseEthNetworkId } from './ethereum';
+import _ from 'lodash'
+import { BlockchainDict, EthBlockchain } from '../entities/Blockchain'
+import { EthNetwork, parseEthNetworkId } from './ethereum'
 
 export function parseBlockchains(params?: Record<string, any>): BlockchainDict {
   const ethereumNetworkId = parseEthNetworkId(_.get(params, 'ethereum', EthNetwork.MAIN))
