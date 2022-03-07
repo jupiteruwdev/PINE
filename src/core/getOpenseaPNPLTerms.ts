3fa271162efa38bf53e10ce369d42c74a529a9c9
Alexander Ho <39906305+piske-alex@users.noreply.github.com>
2022-03-07 19:39:33 +0000

    submit account address to opensea

--- src/core/getOpenseaPNPLTerms.ts
+++ src/core/getOpenseaPNPLTerms.ts
@@ -17,7 +17,7 @@ const openseaContractAddresses = {
   main: '0x7Be8076f4EA4A4AD08075C2508e481d6C946D12b',
 }
 
-const flashLoanSourceContractAddress: { [key: number]: any } = {
+const flashLoanSourceContractAddresses: { [key: number]: any } = {
   4: '0x8eE816b1B3B3E5F2dE1d8344A7Dc69AA16074314',
   // TODO: add mainnet flashloan contract
 }
@@ -28,12 +28,13 @@ export default async function getOpenseaPNPLTerms({ openseaVersion, blockchain, 
   switch (blockchain.network) {
   case 'ethereum': {
     const loanTerms = await getLoanTerms({ blockchain, collectionId, nftId })
-    const { data: openseaInstructions } = await axios.get(`https://us-central1-pinedefi.cloudfunctions.net/opensea-purchase-generator?nft_address=${loanTerms.collection.address}&token_id=${nftId}&network_name=${openseaVersion}`)
+    const flashLoanSourceContractAddress = flashLoanSourceContractAddresses[Number(blockchain.networkId)]
+    const { data: openseaInstructions } = await axios.get(`https://us-central1-pinedefi.cloudfunctions.net/opensea-purchase-generator?nft_address=${loanTerms.collection.address}&token_id=${nftId}&network_name=${openseaVersion}&account_address=${flashLoanSourceContractAddress}`)
     const pnplTerms: PNPLTerms = {
       ...loanTerms,
       purchaseInstruction: openseaInstructions.calldata,
       listedPrice: openseaInstructions.currentPrice,
-      flashLoanSourceContractAddress: flashLoanSourceContractAddress[Number(blockchain.networkId)],
+      flashLoanSourceContractAddress,
       marketplaceContractAddress: openseaContractAddresses[openseaVersion],
     }
     return pnplTerms
