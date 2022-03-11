b16762a1d0d22349985f60e5554ab574afad6fbd
Alexander Ho <39906305+piske-alex@users.noreply.github.com>
2022-03-11 02:55:00 +0000

    Fix lint

--- src/core/getPoolContract.ts
+++ src/core/getPoolContract.ts
@@ -1,17 +1,16 @@
-import Web3 from 'web3'
 import ERC721LendingABI from '../abis/ERC721Lending.json'
 import ERC721LendingABIV2 from '../abis/ERC721LendingV2.json'
 import Blockchain from '../entities/lib/Blockchain'
 import { getEthWeb3 } from '../utils/ethereum'
 import failure from '../utils/failure'
-import { Contract } from "web3-eth-contract";
+import { Contract } from 'web3-eth-contract'
 
 type Params = {
   blockchain: Blockchain
   poolAddress: string
 }
 
-type VersionedContract = Contract & {poolVersion?: number}
+type VersionedContract = Contract & { poolVersion?: number }
 
 export default async function getPoolContract({ blockchain, poolAddress }: Params) : Promise<VersionedContract> {
   switch (blockchain.network) {
@@ -19,12 +18,12 @@ export default async function getPoolContract({ blockchain, poolAddress }: Param
     const web3 = getEthWeb3(blockchain.networkId)
     const contractTest: VersionedContract = new web3.eth.Contract(ERC721LendingABIV2 as any, poolAddress)
     try {
-      await contractTest.methods._controlPlane().call();
+      await contractTest.methods._controlPlane().call()
       contractTest.poolVersion = 2
       return contractTest
     }
     catch (e) {
-      const contract: VersionedContract = new web3.eth.Contract(ERC721LendingABI as any, poolAddress);
+      const contract: VersionedContract = new web3.eth.Contract(ERC721LendingABI as any, poolAddress)
       contract.poolVersion = 1
       return contract
     }
