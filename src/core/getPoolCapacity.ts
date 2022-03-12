270e249c2f16d182d971a1d3e371df5db7f59b43
Alexander Ho <39906305+piske-alex@users.noreply.github.com>
2022-03-12 02:09:50 +0000

    get accurate TVL for v2 pools

--- src/core/getPoolCapacity.ts
+++ src/core/getPoolCapacity.ts
@@ -2,6 +2,8 @@ import Blockchain from '../entities/lib/Blockchain'
 import Value, { $ETH } from '../entities/lib/Value'
 import { getEthWeb3 } from '../utils/ethereum'
 import failure from '../utils/failure'
+import getPoolContract from './getPoolContract'
+import getTokenContract from './getTokenContract'
 
 type Params = {
   blockchain: Blockchain
@@ -13,10 +15,22 @@ export default async function getPoolCapacity({ blockchain, poolAddress }: Param
   case 'ethereum': {
     try {
       const web3 = getEthWeb3(blockchain.networkId)
-      const balanceWei = await web3.eth.getBalance(poolAddress)
-      const balanceEth = web3.utils.fromWei(balanceWei)
-
-      return $ETH(balanceEth)
+      const contract = await getPoolContract({ blockchain, poolAddress })
+      switch (contract.poolVersion) {
+        case 1:
+          const balanceWei = await web3.eth.getBalance(poolAddress)
+          const balanceEth = web3.utils.fromWei(balanceWei)
+          return $ETH(balanceEth)
+        case 2:
+          const tokenAddress = await contract.methods._supportedCurrency().call()
+          const tokenContract = getTokenContract({ blockchain, address: tokenAddress })
+          const balanceWethWei = await tokenContract.methods.balanceOf(poolAddress).call()
+          const balanceWEth = web3.utils.fromWei(balanceWethWei)
+          return $ETH(balanceWEth)
+        default:
+          failure('BAD-POOL-VERSION')
+      }
+      
     }
     catch (err) {
       throw failure('FETCH_ETH_BALANCE_FAILURE', err)
