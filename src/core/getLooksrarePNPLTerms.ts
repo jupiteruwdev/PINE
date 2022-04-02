6ad3f128b70311ff2b89df997889051ae572117b
alex-at-pine <96759630+alex-at-pine@users.noreply.github.com>
2022-04-02 19:48:48 -0400

    looksrare pnpl terms

---
+++ src/core/getLooksrarePNPLTerms.ts
@@ -0,0 +1,66 @@
+import axios, { AxiosError } from 'axios'
+import BigNumber from 'bignumber.js'
+import Blockchain from '../entities/lib/Blockchain'
+import PNPLTerms from '../entities/lib/PNPLTerms'
+import { $WEI } from '../entities/lib/Value'
+import failure from '../utils/failure'
+import logger from '../utils/logger'
+import getFlashLoanSourceContractAddress from './getFlashLoanSourceContractAddress'
+import getLoanTerms from './getLoanTerms'
+import getPoolContract from './getPoolContract'
+
+type Params = {
+  blockchain: Blockchain
+  collectionId: string
+  nftId: string
+}
+
+const looksrareContractAddresses: { [key: number]: any } = {
+  4: '0x1AA777972073Ff66DCFDeD85749bDD555C0665dA',
+  1: '0x59728544B08AB483533076417FbBB2fD0B17CE3a',
+}
+
+const pnplContractAddresses: { [key: number]: any } = {
+  4: '0x7D33BdDfe5945687382625547aBD8a0115B87490',
+  1: '0xaD67300C087eC6c8Fb379671A418e77D79214beE',
+}
+
+export default async function getLooksrarePNPLTerms({ blockchain, collectionId, nftId }: Params): Promise<PNPLTerms> {
+  logger.info(`Fetching Looksrare PNPL terms for NFT ID <${ nftId }> and collection ID <${ collectionId }> on blockchain <${ JSON.stringify(blockchain) }>...`)
+
+  switch (blockchain.network) {
+  case 'ethereum': {
+    const loanTerms = await getLoanTerms({ blockchain, collectionId, nftId })
+    const poolContract = await getPoolContract({ blockchain, poolAddress: loanTerms.poolAddress })
+    if ((poolContract.poolVersion || 0) < 2) throw failure('UNSUPPORTED_COLLECTION')
+    const pnplContractAddress = pnplContractAddresses[Number(blockchain.networkId)]
+
+    try {
+      const { data: lookrareInstructions } = await axios.get(`https://northamerica-northeast1-pinedefi.cloudfunctions.net/looksrare-purchase-generator?nft_address=${loanTerms.collection.address}&token_id=${nftId}&network_id=${blockchain.networkId}&account_address=${pnplContractAddress}`)
+      const flashLoanSourceContractAddress = await getFlashLoanSourceContractAddress({ blockchain, poolAddress: loanTerms.poolAddress, flashLoanAmount: lookrareInstructions.currentPrice })
+      const pnplTerms: PNPLTerms = {
+        ...loanTerms,
+        flashLoanSourceContractAddress,
+        listedPrice: $WEI(new BigNumber(lookrareInstructions.currentPrice)),
+        marketplaceContractAddress: looksrareContractAddresses[Number(blockchain.networkId)],
+        marketplaceName: 'Looksrare',
+        pnplContractAddress,
+        purchaseInstruction: lookrareInstructions.calldata,
+      }
+
+      return pnplTerms
+    }
+    catch (error: any) {
+      if ((error as AxiosError).response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        logger.error(`Failed to get Looksrare calldata ${JSON.stringify(error.response.data)}`)
+        throw failure('FETCH_PNPL_TERMS_FAILURE', error.response.data.details)
+      }
+      throw error
+    }
+  }
+  default:
+    throw failure('UNSUPPORTED_BLOCKCHAIN')
+  }
+}
--- src/core/getPNPLTermsByUrl.ts
+++ src/core/getPNPLTermsByUrl.ts
@@ -5,6 +5,7 @@ import PNPLTerms from '../entities/lib/PNPLTerms'
 import failure from '../utils/failure'
 import Web3 from 'web3'
 import getOpenseaPNPLTerms from './getOpenseaPNPLTerms'
+import getLooksrarePNPLTerms from './getLooksrarePNPLTerms'
 
 type Params = {
   parsedURL: URL
@@ -47,6 +48,40 @@ export default async function getPNPLTermsByUrl({ parsedURL }: Params): Promise<
 
     return getOpenseaPNPLTerms({
       openseaVersion: 'rinkeby',
+      blockchain: EthBlockchain(EthereumNetwork.RINKEBY),
+      collectionId: collection.id,
+      nftId,
+    })
+
+    break
+  }
+  case 'looksrare.org': {
+    // https://looksrare.org/collections/0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949/3060
+    const [, , collectionAddress, nftId] = parsedURL.pathname.split('/')
+    if (!Web3.utils.isAddress(collectionAddress)) throw failure('PNPL_INVALID_URL')
+    if (!collectionAddress || !nftId) throw failure('PNPL_INVALID_URL')
+
+    const collection = await findOneCollection({ address: collectionAddress, blockchain: EthBlockchain(EthereumNetwork.MAIN) })
+    if (!collection) throw failure('UNSUPPORTED_COLLECTION')
+
+    return getLooksrarePNPLTerms({
+      blockchain: EthBlockchain(EthereumNetwork.MAIN),
+      collectionId: collection.id,
+      nftId,
+    })
+
+    break
+  }
+  case 'rinkeby.looksrare.org': {
+    // https://looksrare.org/collections/0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949/3060
+    const [, , collectionAddress, nftId] = parsedURL.pathname.split('/')
+    if (!Web3.utils.isAddress(collectionAddress)) throw failure('PNPL_INVALID_URL')
+    if (!collectionAddress || !nftId) throw failure('PNPL_INVALID_URL')
+
+    const collection = await findOneCollection({ address: collectionAddress, blockchain: EthBlockchain(EthereumNetwork.RINKEBY) })
+    if (!collection) throw failure('UNSUPPORTED_COLLECTION')
+
+    return getLooksrarePNPLTerms({
       blockchain: EthBlockchain(EthereumNetwork.RINKEBY),
       collectionId: collection.id,
       nftId,
