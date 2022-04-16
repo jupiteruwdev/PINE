d4912fc4ac0897becae593070346b9ffb4d80fcd
alex-at-pine <96759630+alex-at-pine@users.noreply.github.com>
2022-04-16 14:54:57 -0400

    use parseEthNetworkIc

--- src/routes/collections.ts
+++ src/routes/collections.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express'
-import _ from 'lodash'
 import getAggregatedPools from '../core/getAggregatedPools'
 import { serializeAggregatedPool } from '../entities/lib/AggregatedPool'
-import EthereumNetwork from '../entities/lib/EthereumNetwork'
 import { parseEthNetworkId } from '../utils/ethereum'
 import failure from '../utils/failure'
 
 const router = Router()
 
 router.get('/ethereum/:collectionAddress/pools', async (req, res, next) => {
-  const networkId = _.toNumber(req.query.networkId ?? EthereumNetwork.MAIN)
+  const networkId = parseEthNetworkId(req.query.networkId)
   const collectionAddress = req.params.collectionAddress
 
   try {
-    const pools = await getAggregatedPools({ blockchains: { ethereum: parseEthNetworkId(networkId) }, collectionAddress })
+    const pools = await getAggregatedPools({ blockchains: { ethereum: networkId }, collectionAddress })
     const payload = serializeAggregatedPool(pools[0])
     res.status(200).json(payload)
   }
