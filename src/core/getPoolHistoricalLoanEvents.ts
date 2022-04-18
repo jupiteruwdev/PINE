b387b3c131894314440024837da1a5ff7c59b739
Andrew Wei <andrewscwei@gmail.com>
2022-04-18 15:11:39 -0700

    Better error reporting

--- src/core/getPoolHistoricalLoanEvents.ts
+++ src/core/getPoolHistoricalLoanEvents.ts
@@ -10,18 +10,9 @@ type Params = {
 export async function getPoolHistoricalLoanEvents({ blockchain, poolAddress }: Params) {
   switch (blockchain.network) {
   case 'ethereum': {
-    try {
-      const poolContract = await getPoolContract({ blockchain, poolAddress })
-      const events = await poolContract.getPastEvents('LoanInitiated', {
-        fromBlock: 0,
-        toBlock: 'latest',
-      })
-
-      return events
-    }
-    catch (err) {
-      throw failure('FETCH_POOL_EVENTS_FAILURE', err)
-    }
+    const poolContract = await getPoolContract({ blockchain, poolAddress }).catch(err => { throw failure('FETCH_POOL_CONTRACT', err) })
+    const events = await poolContract.getPastEvents('LoanInitiated', { fromBlock: 0, toBlock: 'latest' }).catch(err => { throw failure('FETCH_POOL_EVENTS_FAILURE', err) })
+    return events
   }
   default:
     throw failure('UNSUPPORTED_BLOCKCHAIN')
