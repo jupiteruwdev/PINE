56b294264fd68f7d656d9d1c9a8a8697e5fd6439
alex-at-pine <96759630+alex-at-pine@users.noreply.github.com>
2022-05-15 18:58:48 +0800

    get all obligations with graph instead of loop

--- src/core/getObligations.ts
+++ src/core/getObligations.ts
@@ -1,25 +1,44 @@
+import axios from 'axios'
 import _ from 'lodash'
 import { findAll as findAllPools } from '../db/pools'
 import Blockchain from '../entities/lib/Blockchain'
 import failure from '../utils/failure'
+import getRequest from '../utils/getRequest'
 import getLoanEvent from './getLoanEvent'
 import getNFTMetadata from './getNFTMetadata'
 import getNFTsByOwner from './getNFTsByOwner'
+import { findOne as findOneCollection } from '../db/collections'
+import NFT from '../entities/lib/NFT'
 
 type Params = {
   blockchain: Blockchain
   borrowerAddress: string
 }
 
+const APIURL = 'https://api.thegraph.com/subgraphs/name/pinedefi/open-loans'
+
+const tokensQuery = (borrower: string) => JSON.stringify(
+{
+  "operationName": "openLoans",
+  "query": `query {
+    loans(where: {borrower: "${borrower}"}) {
+      erc721
+      id
+    }
+  }`,
+  "variables": {}
+})
+
 export default async function getObligations({ blockchain, borrowerAddress }: Params) {
   const pools = await findAllPools({ blockchains: { [blockchain.network]: blockchain.networkId }, includeRetired: true })
-  const allCollaterals = _.flatten(await Promise.all(pools.map((pool, index) => getNFTsByOwner({ blockchain, ownerAddress: pool.address, populateMetadata: false, index }))))
-  const allEvents = await Promise.all(allCollaterals.map(collateral => {
-    if (!collateral.ownerAddress) throw failure('FETCH_LOAN_EVENTS_FAILURE')
-    return getLoanEvent({ blockchain, nftId: collateral.id, poolAddress: collateral.ownerAddress })
+  const { data: {data: { loans } } } = await axios.post(APIURL, tokensQuery(borrowerAddress))
+  console.log(loans)
+  const nfts: NFT[] = await Promise.all(loans.map(async (loan: any) => {
+    return {
+      collection: await findOneCollection({ address: loan.erc721 }),
+      id: loan.id.split('/')[1]
+    }
   }))
-
-  const nfts = _.compact(allEvents.map((event, idx) => borrowerAddress.toLowerCase() !== _.get(event, 'borrower')?.toLowerCase() ? undefined : allCollaterals[idx]))
 
   // TODO: Optimize this. Currently doing this in series to avoid 429 for some API calls.
   for (let i = 0, n = nfts.length; i < n; i++) {
