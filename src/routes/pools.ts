4b533006217e8e3553755594bdd32b780e3137f4
jupiteruwdev <harutoinoue8@gmail.com>
2022-06-02 10:58:22 +0200

    collections total count issue fixed.

--- src/routes/pools.ts
+++ src/routes/pools.ts
@@ -3,7 +3,7 @@ import _ from 'lodash'
 import getAggregatedPools from '../core/getAggregatedPools'
 import getPool from '../core/getPool'
 import getPools from '../core/getPools'
-import { totalCount } from '../db/collections'
+import { findAll } from '../db/collections'
 import { serializeAggregatedPools } from '../entities/lib/AggregatedPool'
 import { EthBlockchain, SolBlockchain } from '../entities/lib/Blockchain'
 import EthereumNetwork from '../entities/lib/EthereumNetwork'
@@ -16,7 +16,7 @@ import mapBlockchainFilterToDict from '../utils/mapBlockchainFilterToDict'
 const router = Router()
 
 router.get('/', async (req, res, next) => {
-  const collectionCount = totalCount()
+  const totalCount = (await findAll()).length
   const networkName = req.query.networkName?.toString()
   const blockchain = networkName === 'solana' ? SolBlockchain(req.query.networkId?.toString()) : EthBlockchain(parseEthNetworkId(req.query.networkId))
   const collectionAddress = req.query.collectionAddress?.toString().toLowerCase()
@@ -34,7 +34,7 @@ router.get('/', async (req, res, next) => {
     })
     const payload = serializePools(pools)
     const nextOffset = (offset ?? 0) + pools.length
-    const pagination = serializePagination({ data: payload, totalCount: collectionCount, nextOffset: nextOffset === collectionCount - 1 ? undefined : nextOffset })
+    const pagination = serializePagination({ data: payload, totalCount, nextOffset: nextOffset === totalCount - 1 ? undefined : nextOffset })
     res.status(200).json(pagination)
   }
   else {
@@ -42,7 +42,7 @@ router.get('/', async (req, res, next) => {
       const pools = await getAggregatedPools({ blockchains: _.mapValues(mapBlockchainFilterToDict(req.query, true), t => t.networkId), count, offset })
       const payload = serializeAggregatedPools(pools)
       const nextOffset = (offset ?? 0) + pools.length
-      const pagination = serializePagination({ data: payload, totalCount: collectionCount, nextOffset: nextOffset === collectionCount - 1 ? undefined : nextOffset })
+      const pagination = serializePagination({ data: payload, totalCount, nextOffset: nextOffset === totalCount - 1 ? undefined : nextOffset })
       res.status(200).json(pagination)
     }
     catch (err) {
