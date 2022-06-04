f1bf241e601f99427e6f618e58bf014e08f60201
Andrew Wei <andrewscwei@gmail.com>
2022-06-04 00:54:08 -0700

    Fix knock-on bug when fetching aggregated pools by collection address

--- src/routes/collections.ts
+++ src/routes/collections.ts
@@ -2,16 +2,17 @@ import { Router } from 'express'
 import getAggregatedPools from '../core/getAggregatedPools'
 import { serializeAggregatedPool } from '../entities/lib/AggregatedPool'
 import failure from '../utils/failure'
-import { getBlockchainFilter, getString } from '../utils/query'
+import { getBlockchainFilter } from '../utils/query'
 
 const router = Router()
 
 router.get('/ethereum/:collectionAddress/pools', async (req, res, next) => {
   try {
-    const collectionAddress = getString(req.query, 'collectionAddress')
+    const collectionAddress = req.params.collectionAddress
     const blockchainFilter = getBlockchainFilter(req.query, true)
     const pools = await getAggregatedPools({ blockchainFilter, collectionAddress })
     const payload = serializeAggregatedPool(pools[0])
+
     res.status(200).json(payload)
   }
   catch (err) {
