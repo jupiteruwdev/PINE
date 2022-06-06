f0f0e8adb5053862ad72562ed5e7d7fb2d396a75
alex-at-pine <96759630+alex-at-pine@users.noreply.github.com>
2022-06-06 16:24:27 -0400

    Fix getRolloverLoan for rinkeby

--- src/core/getExistingLoan.ts
+++ src/core/getExistingLoan.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { findOne as findOneCollection } from '../db/collections'
+import getLoanPosition from './getLoanPosition'
 import Blockchain from '../entities/lib/Blockchain'
+import EthereumNetwork from '../entities/lib/EthereumNetwork'
 import failure from '../utils/failure'
 import logger from '../utils/logger'
 
@@ -33,7 +35,14 @@ export default async function getExistingLoan({ blockchain, collectionId, nftId 
   case 'ethereum': {
     const collection = await findOneCollection({ id: collectionId, blockchain })
     if (!collection) throw failure('UNSUPPORTED_COLLECTION')
-
+    if (blockchain.networkId === EthereumNetwork.RINKEBY) {
+      const position = await getLoanPosition({ blockchain, collectionId, nftId, txSpeedBlocks: 0 })
+      return {
+        borrowedWei: position?.borrowed.amount.toString(),
+        returnedWei: position?.returned.amount.toString(),
+        pool: position?.poolAddress,
+      }
+    }
     const { data: { data: { loans } } } = await axios.post(APIURL, tokensQuery(collection.address, nftId))
     const existingLoan = loans.length > 0 ? loans[0] : undefined
 
