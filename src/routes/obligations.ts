8f78585acc602ff1645f983c4bd1375f89cd2416
jupiteruwdev <harutoinoue8@gmail.com>
2022-06-07 23:32:39 +0200

    Fix: Update query get blockchain strictness

--- src/routes/obligations.ts
+++ src/routes/obligations.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import getObligation from '../core/getObligation'
 import getObligations from '../core/getObligations'
 import { serializeActiveLoanStats } from '../entities/lib/ActiveLoanStat'
+import { EthBlockchain } from '../entities/lib/Blockchain'
 import { serializeNFTs } from '../entities/lib/NFT'
 import failure from '../utils/failure'
 import { getBlockchain, getString } from '../utils/query'
+import tryOrUndefined from '../utils/tryOrUndefined'
 
 const router = Router()
 
@@ -25,7 +27,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:collectionAddress', async (req, res, next) => {
   try {
     const collectionAddress = req.params.collectionAddress
-    const blockchain = getBlockchain(req.query)
+    const blockchain = tryOrUndefined(() => getBlockchain(req.query)) ?? EthBlockchain()
     const obligation = await getObligation({ collectionAddress, blockchain })
     const payload = serializeActiveLoanStats(obligation)
     res.status(200).json(payload)
