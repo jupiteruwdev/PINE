160265d56a0a9227a18a6bc8b02579dee0d70d39
demu <demu@pine.loans>
2022-06-29 11:15:17 +0900

    Update tests

--- src/routes/stats.spec.ts
+++ src/routes/stats.spec.ts
@@ -4,15 +4,17 @@ import request from 'supertest'
 import app from '../app'
 
 describe('routes/stats', () => {
-  it('can get global stats', async () => {
-    const { body: res } = await request(app).get('/stats/global')
-      .expect('Content-Type', /json/)
-      .expect(200)
+  describe('GET /stats/global', () => {
+    it('can get global stats', async () => {
+      const { body: res } = await request(app).get('/stats/global')
+        .expect('Content-Type', /json/)
+        .expect(200)
 
-    expect(res).to.have.property('capacity')
-    expect(res).to.have.property('totalValueLentHistorical')
-    expect(res).to.have.property('totalValueLocked')
-    expect(res).to.have.property('utilizationRatio')
-    expect(res).to.have.property('utilization')
+      expect(res).to.have.property('capacity')
+      expect(res).to.have.property('totalValueLentHistorical')
+      expect(res).to.have.property('totalValueLocked')
+      expect(res).to.have.property('utilizationRatio')
+      expect(res).to.have.property('utilization')
+    })
   })
 })
