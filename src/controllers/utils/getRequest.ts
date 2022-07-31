033ffaca8f85681be33347f3d11f7668bdddfb64
demu <demu@pine.loans>
2022-07-30 23:56:41 -0700

    Improve logging of getRequest

--- src/controllers/utils/getRequest.ts
+++ src/controllers/utils/getRequest.ts
@@ -22,23 +22,26 @@ export default async function getRequest<T = any>(path: string, { controller, ho
 
     const payload = transformPayload ? transformPayload(res.data) : res.data
 
-    logger.info(`Making request to <${host ?? ''}${path}>... ${res.status}`, payload)
+    logger.info(`Making request to <${host ?? ''}${path}>... ${res.status}`)
+    // logger.debug(JSON.stringify(payload, undefined, 2))
 
     return payload
   }
   catch (err) {
     if (axios.isCancel(err)) {
-      logger.warn(`Making request to <${host ?? ''}${path}>... SKIP:`, err)
+      logger.warn(`Making request to <${host ?? ''}${path}>... SKIP`)
       throw fault('ERR_REQUEST_CANCELLED', undefined, err)
     }
     else if (axios.isAxiosError(err)) {
-      const error = err.response?.status === undefined ? fault('ERR_SYSTEM_OFFLINE') : new SuperError(err.message, err.response?.status.toString(), err.response?.data as any, err)
-      logger.error(`Making request to <${host ?? ''}${path}>... ${err.response?.status ?? 'ERR:'}`, error)
+      const error = err.response?.status === undefined ? fault('ERR_SYSTEM_OFFLINE') : new SuperError(err.message, err.response?.status.toString(), undefined, err.response?.data as any)
+      logger.error(`Making request to <${host ?? ''}${path}>... ${err.response?.status ?? 'ERR'}`)
+      console.error(error)
       throw error
     }
     else {
-      const error = err instanceof TypeError ? err : fault('ERR_UNEXPECTED_PAYLOAD')
-      logger.error(`Making request to <${host ?? ''}${path}>... ERR:`, error)
+      const error = err instanceof TypeError ? err : fault('ERR_UNEXPECTED_PAYLOAD', undefined, err)
+      logger.error(`Making request to <${host ?? ''}${path}>... ERR`)
+      console.error(error)
       throw error
     }
   }
