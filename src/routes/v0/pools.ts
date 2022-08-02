0a773dd0f9dd2301cb793b4f183883060397fd95
jupiteruwdev <harutoinoue8@gmail.com>
2022-08-02 03:23:40 +0200

    Feat: Add lender pool endpoint pagination

--- src/routes/v0/pools.ts
+++ src/routes/v0/pools.ts
@@ -36,7 +36,7 @@ router.get('/groups/search', async (req, res, next) => {
     const poolGroups = await searchPoolGroups({ collectionAddress, collectionName, blockchainFilter, paginateBy, sortBy })
     const payload = serializeEntityArray(poolGroups, PoolGroup.codingResolver)
     const nextOffset = (paginateBy?.offset ?? 0) + poolGroups.length
-    const pagination = Pagination.serialize({ data: payload, totalCount, nextOffset: nextOffset === totalCount - 1 ? undefined : nextOffset })
+    const pagination = Pagination.serialize({ data: payload, totalCount, nextOffset: nextOffset === totalCount ? undefined : nextOffset })
 
     res.status(200).json(pagination)
   }
@@ -49,6 +49,9 @@ router.get('/lender', async (req, res, next) => {
   try {
     const blockchainFilter = getBlockchainFilter(req.query, true)
     const lenderAddress = getString(req.query, 'lenderAddress')
+    const paginateByOffset = getNumber(req.query, 'offset', { optional: true })
+    const paginateByCount = getNumber(req.query, 'count', { optional: true })
+    const paginateBy = paginateByOffset !== undefined && paginateByCount !== undefined ? { count: paginateByCount, offset: paginateByOffset } : undefined
     const publishedPools = await searchPools({
       blockchainFilter,
       lenderAddress,
@@ -56,9 +59,13 @@ router.get('/lender', async (req, res, next) => {
     const unpublishedPools = await getUnpublishedPoolsByLenderAndAddress({
       blockchainFilter, lenderAddress,
     })
-    const payload = serializeEntityArray([...publishedPools, ...unpublishedPools], Pool.codingResolver)
+    const totalCount = [...publishedPools, ...unpublishedPools].length
+    const currentPagePools = [...publishedPools, ...unpublishedPools].slice(paginateByOffset, (paginateByOffset ?? 0) + (paginateByCount ?? 0))
+    const payload = serializeEntityArray(currentPagePools, Pool.codingResolver)
+    const nextOffset = (paginateBy?.offset ?? 0) + currentPagePools.length
+    const pagination = Pagination.serialize({ data: payload, totalCount, nextOffset: nextOffset === totalCount ? undefined : nextOffset })
 
-    res.status(200).json(payload)
+    res.status(200).json(pagination)
   }
   catch (err) {
     next(fault('ERR_API_FETCH_POOL_BY_LENDER_ADDRESS', undefined, err))
