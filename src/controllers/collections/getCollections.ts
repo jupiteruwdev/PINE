2bf67afa26f79ac57a05258a7e4d5adb3bba2a68
demu <demu@pine.loans>
2022-08-07 10:14:16 -0700

    Refactor getCollections

--- src/controllers/collections/getCollections.ts
+++ src/controllers/collections/getCollections.ts
@@ -1,29 +1,52 @@
 import { NFTCollectionModel } from '../../db'
 import { mapCollection } from '../../db/adapters'
 import { Blockchain, Collection } from '../../entities'
+import composeDataSources, { DataSource } from '../../utils/composeDataSources'
+import logger from '../../utils/logger'
 
 type Params = {
   blockchainFilter?: Blockchain.Filter
 }
 
 export default async function getCollections({
-  blockchainFilter = { ethereum: Blockchain.Ethereum.Network.MAIN, solana: Blockchain.Solana.Network.MAINNET },
+  blockchainFilter = {
+    ethereum: Blockchain.Ethereum.Network.MAIN,
+    solana: Blockchain.Solana.Network.MAINNET,
+  },
 }: Params = {}): Promise<Collection[]> {
-  let collections: Collection[] = []
+  logger.info(`Fetching collections with blockchain filter <${JSON.stringify(blockchainFilter)}>...`)
 
-  if (blockchainFilter.ethereum !== undefined) {
-    const blockchain = Blockchain.Ethereum(blockchainFilter.ethereum)
+  try {
+    const dataSource = composeDataSources(useDb({ blockchainFilter }))
+    const collections = await dataSource.apply(undefined)
 
-    const docs = await NFTCollectionModel.find({
-      networkType: blockchain.network,
-      networkId: blockchain.networkId,
-    }).lean().exec()
+    logger.info(`Fetching collections with blockchain filter <${JSON.stringify(blockchainFilter)}>... OK: Found ${collections.length} result(s)`)
+    logger.debug(JSON.stringify(collections, undefined, 2))
 
-    collections = [
-      ...collections,
-      ...docs.map(mapCollection),
-    ]
+    return collections
   }
+  catch (err) {
+    logger.error(`Fetching collections with blockchain filter <${JSON.stringify(blockchainFilter)}>... ERR`)
+    if (logger.isErrorEnabled() && !logger.silent) console.error(err)
 
-  return collections
+    throw err
+  }
+}
+
+export function useDb({ blockchainFilter }: Required<Params>): DataSource<Collection[]> {
+  return async () => {
+    const networkTypes = Object.keys(blockchainFilter) as (keyof Blockchain.Filter)[]
+    const blockchains = networkTypes.map(networkType => Blockchain.factory({ network: networkType, networkId: blockchainFilter[networkType] }) )
+
+    const res = await Promise.all(blockchains.map(async blockchain => {
+      const docs = await NFTCollectionModel.find({
+        networkType: blockchain.network,
+        networkId: blockchain.networkId,
+      }).lean().exec()
+
+      return docs.map(mapCollection)
+    }))
+
+    return res.reduce<Collection[]>((prev, curr) => [...prev, ...curr], [])
+  }
 }
