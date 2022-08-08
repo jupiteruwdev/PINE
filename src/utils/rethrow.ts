b032adb6112e39d9290ec440c870156a72d05ea6
demu <demu@pine.loans>
2022-08-08 10:28:59 -0700

    Add docs

--- src/utils/rethrow.ts
+++ src/utils/rethrow.ts
@@ -1,5 +1,11 @@
 import SuperError from '@andrewscwei/super-error'
 
+/**
+ * Throws an application fault. Useful for catching a generic {@link Error} and rethrowing it as a
+ * fault, or simply throwing a string as a fault.
+ *
+ * @param error - A value to be deserialized into a {@link SuperError}.
+ */
 export default function rethrow(error: unknown): never {
   throw SuperError.deserialize(error)
 }
