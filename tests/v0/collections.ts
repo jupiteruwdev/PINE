f7b0de47e7f461a49d2dadf720351b479d478a1e
demu <demu@pine.loans>
2022-08-12 16:20:23 -0700

    Add test reports

--- tests/v0/collections.ts
+++ tests/v0/collections.ts
@@ -4,34 +4,38 @@ import app from '../../src/app'
 import { getCollections } from '../../src/controllers'
 import { Blockchain, Value } from '../../src/entities'
 
-describe('GET /v0/collections/floors', () => {
-  it('can get floor prices by colletion addresses on Ethereum Mainnet', async () => {
-    const collections = await getCollections({ blockchainFilter: { ethereum: Blockchain.Ethereum.Network.MAIN } })
+describe('/v0/collections', () => {
+  describe('Ethereum Mainnet', () => {
+    it('GET /v0/collections/floors?collectionAddresses=*', async () => {
+      const collections = await getCollections({ blockchainFilter: { ethereum: Blockchain.Ethereum.Network.MAIN } })
 
-    const { body: res } = await request(app).get('/v0/collections/floors')
-      .query({
-        ethereum: Blockchain.Ethereum.Network.MAIN,
-        collectionAddresses: collections.map(t => t.address),
-      })
-      .expect('Content-Type', /json/)
-      .expect(200)
+      const { body: res } = await request(app).get('/v0/collections/floors')
+        .query({
+          ethereum: Blockchain.Ethereum.Network.MAIN,
+          collectionAddresses: collections.map(t => t.address),
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
 
-    expect(res).to.have.length(collections.length)
-    res.every((t: any) => expect(t).to.have.keys(...Object.keys(Value.codingResolver)))
+      expect(res).to.have.length(collections.length)
+      res.every((t: any) => expect(t).to.have.keys(...Object.keys(Value.codingResolver)))
+    })
   })
 
-  it('can get floor prices by collection addresses on Ethereum Rinkeby', async () => {
-    const collections = await getCollections({ blockchainFilter: { ethereum: Blockchain.Ethereum.Network.RINKEBY } })
+  describe('Ethereum Rinkeby', () => {
+    it('GET /v0/collections/floors?collectionAddresses=*', async () => {
+      const collections = await getCollections({ blockchainFilter: { ethereum: Blockchain.Ethereum.Network.RINKEBY } })
 
-    const { body: res } = await request(app).get('/v0/collections/floors')
-      .query({
-        ethereum: Blockchain.Ethereum.Network.RINKEBY,
-        collectionAddresses: collections.map(t => t.address),
-      })
-      .expect('Content-Type', /json/)
-      .expect(200)
+      const { body: res } = await request(app).get('/v0/collections/floors')
+        .query({
+          ethereum: Blockchain.Ethereum.Network.RINKEBY,
+          collectionAddresses: collections.map(t => t.address),
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
 
-    expect(res).to.have.length(collections.length)
-    res.every((t: any) => expect(t).to.have.keys(...Object.keys(Value.codingResolver)))
+      expect(res).to.have.length(collections.length)
+      res.every((t: any) => expect(t).to.have.keys(...Object.keys(Value.codingResolver)))
+    })
   })
 })
