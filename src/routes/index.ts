53a0f6aea732916b2fc96a4f2deeb6c59c063169
demu <demu@pine.loans>
2022-09-14 12:19:38 -0700

    Add worker communication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
 import appConf from '../app.conf'
+import getWorkerVersion from '../controllers/utils/getWorkerVersion'
 import v0Router from './v0'
 
 const router = Router()
 
 router.get('/health', (req, res) => res.sendStatus(200))
-router.get('/version', (req, res) => res.send(`${appConf.version}/${appConf.build}`))
+
+router.get('/version', async (req, res) => {
+  res.send({
+    core: `${appConf.version}/${appConf.build}`,
+    worker: await getWorkerVersion(),
+  })
+})
+
 router.use('/v0', v0Router)
 router.use('/', v0Router)
 
