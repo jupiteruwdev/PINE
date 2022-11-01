c66bfc7576617750ea61d958b37f50e2acf5a0a2
sbvhev <rohannepal200@gmail.com>
2022-11-01 08:06:57 -0600

    Upgrade gem public api

--- src/controllers/collections/searchCollections.ts
+++ src/controllers/collections/searchCollections.ts
@@ -16,7 +16,7 @@ export default async function searchCollections({ query, blockchain }: Params): 
 
   switch (blockchain.networkId) {
   case Blockchain.Ethereum.Network.MAIN:
-    const collectionData = await postRequest('https://gem-public-api.herokuapp.com/collections',
+    const collectionData = await postRequest('https://gem-public-api-v2.herokuapp.com/collections',
       {
         filters: { searchText: query },
         sort: { sevenDayVolume: -1 },
@@ -26,6 +26,7 @@ export default async function searchCollections({ query, blockchain }: Params): 
         headers: {
           'X-API-KEY': apiKey,
         },
+        timeout: 10000,
       })
     const collections = _.get(collectionData, 'data')
     return collections.filter((cd: any) => cd.chainId === '1' && _.get(cd, 'addresses[0].address') && cd.name && cd.slug).map((cd: any) => Collection.factory({
