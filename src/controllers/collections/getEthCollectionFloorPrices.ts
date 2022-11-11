2f4eb9a556eb10116714dc196ae7dd40cb91bf48
jupiteruwdev <harutoinoue8@gmail.com>
2022-11-10 19:19:03 -0500

    Feat: Update floor price time limitation

--- src/controllers/collections/getEthCollectionFloorPrices.ts
+++ src/controllers/collections/getEthCollectionFloorPrices.ts
@@ -52,7 +52,7 @@ export function useNFTBank({ blockchain, collectionAddresses }: Params): DataSou
         headers: {
           'X-API-Key': apiKey,
         },
-        timeout: 20000, // TODO: Why so slow
+        timeout: 30000, // TODO: Why so slow
       })
 
       if (!_.isArray(res)) rethrow('Unexpected payload while looking up floor prices from NFTBank')
