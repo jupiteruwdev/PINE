5d6a16a8a3ddbc8a95120d52e67c00a42fd3a55a
jupiteruwdev <harutoinoue8@gmail.com>
2022-11-14 09:13:35 -0500

    Feat: Add api v1

--- src/routes/v0/terms.ts
+++ src/routes/v0/terms.ts
@@ -2,23 +2,18 @@ import { Router } from 'express'
 import { getLoanTerms, getPNPLTermsByUrl, getRolloverTerms } from '../../controllers'
 import { LoanTerms, PNPLTerms, RolloverTerms } from '../../entities'
 import fault from '../../utils/fault'
-import { getBlockchain } from '../utils/query'
+import { getBlockchain, getString } from '../utils/query'
 
 const router = Router()
 
 router.get('/borrow', async (req, res, next) => {
   try {
+    const nftId = getString(req.query, 'nftId')
+    const collectionAddress = getString(req.query, 'collectionAddress')
+    const poolAddress = getString(req.query, 'poolAddress', { optional: true })
     const blockchain = getBlockchain(req.query)
-    const nftIds = req.query.nftIds as string[]
-    const collectionAddresses = req.query.collectionAddresses as string[]
-    const poolAddresses = req.query.poolAddresses as string[]
-    if (!nftIds || !collectionAddresses) return res.status(200).json([])
-    if (nftIds.length !== collectionAddresses.length || poolAddresses?.length && poolAddresses?.length !== nftIds.length) {
-      throw fault('ERR_API_FETCH_LOAN_TERMS', undefined, 'Invalid params')
-    }
-
-    const loanTerms = await getLoanTerms({ blockchain, nftIds, collectionAddresses, poolAddresses })
-    const payload = loanTerms.map(loanTerm => LoanTerms.serialize(loanTerm))
+    const loanTerms = await getLoanTerms({ blockchain, nftIds: [nftId], collectionAddresses: [collectionAddress], poolAddresses: poolAddress ? [poolAddress] : undefined })
+    const payload = LoanTerms.serialize(loanTerms[0])
     res.status(200).json(payload)
   }
   catch (err) {
@@ -28,16 +23,12 @@ router.get('/borrow', async (req, res, next) => {
 
 router.get('/rollover', async (req, res, next) => {
   try {
+    const nftId = getString(req.query, 'nftId')
+    const collectionAddress = getString(req.query, 'collectionAddress')
+    const poolAddress = getString(req.query, 'poolAddress', { optional: true })
     const blockchain = getBlockchain(req.query)
-    const nftIds = req.query.nftIds as string[]
-    const collectionAddresses = req.query.collectionAddresses as string[]
-    const poolAddresses = req.query.poolAddresses as string[]
-    if (!nftIds || !collectionAddresses) return res.status(200).json([])
-    if (nftIds.length !== collectionAddresses.length || poolAddresses?.length && poolAddresses?.length !== nftIds.length) {
-      throw fault('ERR_API_FETCH_LOAN_TERMS', undefined, 'Invalid params')
-    }
-    const rolloverTerms = await getRolloverTerms({ blockchain, nftIds, collectionAddresses, poolAddresses })
-    const payload = rolloverTerms.map(rolloverTerm => RolloverTerms.serialize(rolloverTerm))
+    const rolloverTerms = await getRolloverTerms({ blockchain, nftIds: [nftId], collectionAddresses: [collectionAddress], poolAddresses: poolAddress ? [poolAddress] : undefined })
+    const payload = RolloverTerms.serialize(rolloverTerms[0])
 
     res.status(200).json(payload)
   }
@@ -48,19 +39,19 @@ router.get('/rollover', async (req, res, next) => {
 
 router.get('/pnpl', async (req, res, next) => {
   try {
-    const urls = req.query.urls as string[]
-    const poolAddresses = req.query.poolAddresses as string[]
-    const parsedURLs = urls.map(url => new URL(url))
+    const url = getString(req.query, 'url')
+    const poolAddress = getString(req.query, 'poolAddress', { optional: true })
+    const parsedURL = new URL(url)
     let pnplTerms
 
     try {
-      pnplTerms = await getPNPLTermsByUrl({ parsedURLs, poolAddresses })
+      pnplTerms = await getPNPLTermsByUrl({ parsedURLs: [parsedURL], poolAddresses: poolAddress ? [poolAddress] : undefined })
     }
     catch (err) {
       return next(err)
     }
 
-    const payload = pnplTerms.map(pnplTerm => PNPLTerms.serialize(pnplTerm))
+    const payload = PNPLTerms.serialize(pnplTerms[0])
 
     res.status(200).json(payload)
   }
