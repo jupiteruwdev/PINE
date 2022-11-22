6e2dead15cf87795e44487e86ab49bdbaa6be849
hiroshi <blackbox0823@gmail.com>
2022-11-23 08:51:11 +0900

    add empty line

--- src/routes/v0/pools.ts
+++ src/routes/v0/pools.ts
@@ -40,6 +40,7 @@ router.get('/groups/search', async (req, res, next) => {
     const payload = serializeEntityArray(poolGroups, PoolGroup.codingResolver)
     const nextOffset = (paginateBy?.offset ?? 0) + poolGroups.length
     const pagination = Pagination.serialize({ data: payload, totalCount, nextOffset: nextOffset === totalCount - 1 ? undefined : nextOffset })
+
     res.status(200).json(pagination)
   }
   catch (err) {
