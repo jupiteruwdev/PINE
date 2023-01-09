0dc059d81079f558ba2ab88d4aef88061b627694
jupiteruwdev <harutoinoue8@gmail.com>
2023-01-09 14:09:16 -0500

    Feat: Add user missions api

--- src/routes/v0/stats.ts
+++ src/routes/v0/stats.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getGlobalStats } from '../../controllers'
+import { getGlobalStats, getUserMissionStats } from '../../controllers'
 import { GlobalStats } from '../../entities'
+import UserMissionStats from '../../entities/lib/UserMissionStats'
 import fault from '../../utils/fault'
-import { getBlockchainFilter } from '../utils/query'
+import { getBlockchain, getBlockchainFilter, getNumber, getString } from '../utils/query'
 
 const router = Router()
 
@@ -16,6 +17,22 @@ router.get('/global', async (req, res, next) => {
   }
   catch (err) {
     next(fault('ERR_API_FETCH_GLOBAL_STATS', undefined, err))
+  }
+})
+
+router.get('/user/:address', async (req, res, next) => {
+  try {
+    const blockchain = getBlockchain(req.query)
+    const address = getString(req.params, 'address')
+    const timestamp = getNumber(req.query, 'timestamp', { optional: true })
+
+    const stats = await getUserMissionStats({ blockchain, address, timestamp })
+    const payload = UserMissionStats.serialize(stats)
+
+    res.status(200).json(payload)
+  }
+  catch (err) {
+    next(fault('ERR_API_FETCH_USER_STATS', undefined, err))
   }
 })
 
--- src/subgraph/index.ts
+++ src/subgraph/index.ts
@@ -1,5 +1,8 @@
 export { default as getOnChainGlobalStats } from './getOnChainGlobalStats'
 export { default as getOnChainLoanById } from './getOnChainLoanById'
+export { default as getOnChainLoanByBorrowerAddress } from './getOnChainLoanHistoriesByBorrowerAddress'
 export { default as getOnChainLoans } from './getOnChainLoans'
 export { default as getOnChainPoolByAddress } from './getOnChainPoolByAddress'
 export { default as getOnChainPools } from './getOnChainPools'
+export { default as getOnChainPoolsByLenderAddress } from './getOnChainPoolsByLenderAddress'
+export { default as getPNPLHistoriesByBorrowerAddress } from './getPNPLHistoriesByBorrowerAddress'
