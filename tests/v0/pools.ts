438440023aae150998dce492a301db0a9dbc9b04
jupiteruwdev <harutoinoue8@gmail.com>
2023-01-30 21:06:27 -0500

    Hotfix: Update pools pagination tests

--- tests/v0/pools.ts
+++ tests/v0/pools.ts
@@ -87,7 +87,7 @@ describe('/v0/pools', () => {
 
         if (res.data.length === 1) {
           expect(res.totalCount).to.equal(1)
-          expect(res.nextOffset).to.equal(1)
+          expect(res.nextOffset).to.equal(undefined)
           res.data.every((poolGroup: any) => expect(poolGroup).to.have.keys(...Object.keys(PoolGroup.codingResolver)))
         }
       }))
@@ -108,7 +108,7 @@ describe('/v0/pools', () => {
 
       if (res.data.length === 1) {
         expect(res.totalCount).to.equal(1)
-        expect(res.nextOffset).to.equal(1)
+        expect(res.nextOffset).to.equal(undefined)
         res.data.every((poolGroup: any) => expect(poolGroup).to.have.keys(...Object.keys(PoolGroup.codingResolver)))
       }
     })
