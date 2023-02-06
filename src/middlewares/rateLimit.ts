96c3260c0238e20f9d95cb146a41d4befb7871ce
jupiteruwdev <harutoinoue8@gmail.com>
2023-02-05 21:16:19 -0500

    Feat: Add rate limit middleware

---
+++ src/middlewares/rateLimit.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+import { RateLimiterMemory } from 'rate-limiter-flexible'
+import fault from '../utils/fault'
+
+const opts = {
+  points: 10, // 6 points
+  duration: 1, // Per second
+}
+
+const rateLimiter = new RateLimiterMemory(opts)
+
+export default function rateLimitMiddleware(req: Request, res: Response, next: NextFunction) {
+  const remoteAddress = req.connection.remoteAddress as string
+  rateLimiter.consume(remoteAddress) // consume 2 points
+    .then(rateLimiterRes => {
+      // 2 points consumed
+      next()
+    })
+    .catch(rateLimiterRes =>
+      // Not enough points to consume
+      res.status(429).send({
+        error: fault('TOO_MANY_REQUESTS', undefined),
+      })
+    )
+}
