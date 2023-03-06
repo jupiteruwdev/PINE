d6ee095e3ce0a73b67e7d4d81a1c92f85fc51ec8
jupiteruwdev <harutoinoue8@gmail.com>
2023-03-06 06:40:09 -0500

    Feat: Update user protocol usage with incentives values

--- src/controllers/stats/getUserUsageStats.ts
+++ src/controllers/stats/getUserUsageStats.ts
@@ -1,8 +1,9 @@
 import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
 import _ from 'lodash'
-import { BorrowSnapshotModel } from '../../db'
+import { BorrowSnapshotModel, LendingSnapshotModel } from '../../db'
 import { Value } from '../../entities'
+import { ProtocolUsage } from '../../entities/lib'
 import logger from '../../utils/logger'
 
 type Params = {
@@ -16,22 +17,34 @@ export default async function getUserUsageStats({
     logger.info(`Fetching user protocol usage stats for address ${address}...`)
     const now = new Date()
     now.setHours(now.getHours() - 1)
-    const allSnapshots = await BorrowSnapshotModel.find({ updatedAt: {
+    const allBorrowingSnapshots = await BorrowSnapshotModel.find({ updatedAt: {
       $gt: now,
     } }).lean()
+    const lendingSnapshots = await LendingSnapshotModel.find({ lenderAddress: {
+      $regex: address,
+      $options: 'i',
+    } }).lean()
 
-    const borrowedSnapshots = allSnapshots.filter(snapshot => _.get(snapshot, 'borrowerAddress')?.toLowerCase() === address.toLowerCase())
-    const lendedSnapshots = allSnapshots.filter(snapsoht => _.get(snapsoht, 'lenderAddress')?.toLowerCase() === address.toLowerCase())
+    const borrowedSnapshots = allBorrowingSnapshots.filter(snapshot => _.get(snapshot, 'borrowerAddress')?.toLowerCase() === address.toLowerCase())
+    const lendedSnapshots = allBorrowingSnapshots.filter(snapsoht => _.get(snapsoht, 'lenderAddress')?.toLowerCase() === address.toLowerCase())
 
-    const totalAmount = _.reduce(allSnapshots, (pre, curr) => pre.plus(new BigNumber(_.get(curr, 'borrowAmount', 0))), new BigNumber(0))
-    const usageEth = _.reduce(_.uniqBy([...borrowedSnapshots, ...lendedSnapshots], 'borrowerAddress'), (pre, curr) => pre.plus(new BigNumber(_.get(curr, 'borrowAmount', 0))), new BigNumber(0))
+    const totalAmount = _.reduce(allBorrowingSnapshots, (pre, cur) => pre.plus(new BigNumber(_.get(cur, 'borrowAmount', 0))), new BigNumber(0))
+
+    const borrowedEth = _.reduce(borrowedSnapshots, (pre, cur) => pre.plus(cur.borrowAmount ?? '0'), new BigNumber(0))
+    const lendedEth = _.reduce(lendedSnapshots, (pre, cur) => pre.plus(cur.borrowAmount ?? '0'), new BigNumber(0))
+
+    const usageEth = borrowedEth.plus(lendedEth)
+    const ethCapacity = _.reduce(lendingSnapshots, (pre, cur) => pre.plus(cur.capacity ?? '0'), new BigNumber(0))
 
     logger.info(`Fetching user protocol usage stats for address ${address}... OK`)
 
-    return {
+    return ProtocolUsage.factory({
       usageEth: Value.$ETH(ethers.utils.formatEther(usageEth.toString()).toString()),
       usagePercent: usageEth.div(totalAmount),
-    }
+      borrowedEth: Value.$ETH(ethers.utils.formatEther(borrowedEth.toString()).toString()),
+      lendedEth: Value.$ETH(ethers.utils.formatEther(lendedEth.toString()).toString()),
+      ethCapacity: Value.$ETH(ethCapacity),
+    })
   }
   catch (err) {
     logger.info(`Fetching user protocol usage stats for address ${address}... ERR:`, err)
