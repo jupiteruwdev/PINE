9ae852165e2ad63f3ccb3bf52ce683d5af793c33
jupiteruwdev <harutoinoue8@gmail.com>
2023-03-08 12:44:15 -0500

    Feat: Update incentives endpoint

--- src/controllers/stats/getUserUsageStats.ts
+++ src/controllers/stats/getUserUsageStats.ts
@@ -1,10 +1,12 @@
 import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
 import _ from 'lodash'
+import appConf from '../../app.conf'
 import { BorrowSnapshotModel, LendingSnapshotModel } from '../../db'
 import { Value } from '../../entities'
-import { ProtocolUsage } from '../../entities/lib'
+import { Blockchain, ProtocolUsage } from '../../entities/lib'
 import logger from '../../utils/logger'
+import { getTokenContract } from '../contracts'
 
 type Params = {
   address: string
@@ -23,7 +25,14 @@ export default async function getUserUsageStats({
     const lendingSnapshots = await LendingSnapshotModel.find({ lenderAddress: {
       $regex: address,
       $options: 'i',
+    }, updatedAt: {
+      $gt: now,
     } }).lean()
+
+    const wethContract = getTokenContract({ blockchain: {
+      network: 'ethereum',
+      networkId: Blockchain.Ethereum.Network.MAIN,
+    }, address: _.get(appConf.wethAddress, Blockchain.Ethereum.Network.MAIN) })
 
     const borrowedSnapshots = allBorrowingSnapshots.filter(snapshot => _.get(snapshot, 'borrowerAddress')?.toLowerCase() === address.toLowerCase())
     const lendedSnapshots = allBorrowingSnapshots.filter(snapsoht => _.get(snapsoht, 'lenderAddress')?.toLowerCase() === address.toLowerCase())
@@ -35,6 +44,11 @@ export default async function getUserUsageStats({
 
     const usageEth = borrowedEth.plus(lendedEth)
     const ethCapacity = _.reduce(lendingSnapshots, (pre, cur) => pre.plus(cur.capacity ?? '0'), new BigNumber(0))
+    const wethBalances = await Promise.all(_.uniqBy(lendingSnapshots, 'fundSource').map(snapshot => wethContract.methods.balanceOf(snapshot.fundSource).call()))
+    const wethBalance = _.reduce(wethBalances, (pre, cur) => pre.plus(new BigNumber(cur)), new BigNumber(0))
+    const ethPermissioned = _.min([ethCapacity, new BigNumber(ethers.utils.formatEther(wethBalance.toString()))]) ?? new BigNumber(0)
+
+    const collateralPriceSum = _.reduce(borrowedSnapshots, (pre, cur) => pre.plus(new BigNumber(cur.collateralPrice?.amount ?? '0')), new BigNumber(0))
 
     logger.info(`Fetching user protocol usage stats for address ${address}... OK`)
 
@@ -43,7 +57,8 @@ export default async function getUserUsageStats({
       usagePercent: usageEth.div(totalAmount),
       borrowedEth: Value.$ETH(ethers.utils.formatEther(borrowedEth.toString()).toString()),
       lendedEth: Value.$ETH(ethers.utils.formatEther(lendedEth.toString()).toString()),
-      ethCapacity: Value.$ETH(ethCapacity),
+      ethCapacity: Value.$ETH(ethPermissioned.toString()),
+      collateralPrice: Value.$ETH(collateralPriceSum.toString()),
     })
   }
   catch (err) {
