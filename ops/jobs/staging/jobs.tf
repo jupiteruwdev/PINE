d7a129ace52de0ab53e0a6bf051ebe98f226aa68
jupiteruwdev <harutoinoue8@gmail.com>
2023-03-21 13:51:37 -0400

    Feat: Hide jobs

--- ops/jobs/staging/jobs.tf
+++ ops/jobs/staging/jobs.tf
@@ -1,16 +1,16 @@
-resource "google_cloud_scheduler_job" "job" {
-  name             = "core-service-version-${local.env}"
-  description      = "Test HTTP version trigger"
-  schedule         = "* * * * *"
-  attempt_deadline = "320s"
+# resource "google_cloud_scheduler_job" "job" {
+#   name             = "core-service-version-${local.env}"
+#   description      = "Test HTTP version trigger"
+#   schedule         = "* * * * *"
+#   attempt_deadline = "320s"
 
-  retry_config { retry_count = 1 }
+#   retry_config { retry_count = 1 }
 
-  http_target {
-    http_method = "GET"
-    uri         = "https://core-service-${local.env}.pine.loans/version"
-  }
-}
+#   http_target {
+#     http_method = "GET"
+#     uri         = "https://core-service-${local.env}.pine.loans/version"
+#   }
+# }
 
 # resource "google_cloud_scheduler_job" "syncPools" {
 #   name             = "core-service-sync-pools-${local.env}"
