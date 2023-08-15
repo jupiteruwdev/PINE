3a3af1317d74b4524ea7b17f94cb88198fb1febf
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-08-15 15:32:42 +0300

    feat: add unit tests for coingecko

--- src/controllers/utils/getPineValueUSD.spec.ts
+++ src/controllers/utils/getPineValueUSD.spec.ts
@@ -9,14 +9,9 @@ describe('utils', () => {
     code?: string
   }
 
-  beforeEach(async () => {
-    const { getRequest } = await esmock('./getPineValueUSD', {
-      './getRequest': (getRequestStub = async () => ({ amount: new BigNumber(1000), currency: 'USD' })),
-    })
-  })
+  beforeEach(async () => {})
 
   afterEach(() => {
-    // Clean up mock state after each test
     esmock.purge('./getRequest')
   })
 
@@ -24,7 +19,6 @@ describe('utils', () => {
     it('should fetch PINE value of 2 pine token using gate.io', async () => {
       const fakeData = { quoteVolume: '3686627.3574516', baseVolume: '44655.845625521', highestBid: '0.012036', high24hr: '0.012181', last: '0.01204', lowestAsk: '0.012042', elapsed: '6ms', result: 'true', low24hr: '0.012036', percentChange: '-0.82' }
       getRequestStub = async () => fakeData
-
       const { useGateIO } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
       const resultFunction = await useGateIO(2)
       const result = await resultFunction()
@@ -37,7 +31,6 @@ describe('utils', () => {
     it('should fetch PINE value without params using gate.io', async () => {
       const fakeData = { quoteVolume: '3686627.3574516', baseVolume: '44655.845625521', highestBid: '0.012036', high24hr: '0.012181', last: '0.01204', lowestAsk: '0.012042', elapsed: '6ms', result: 'true', low24hr: '0.012036', percentChange: '-0.82' }
       getRequestStub = async () => fakeData
-
       const { useGateIO } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
       const resultFunction = await useGateIO()
       const result = await resultFunction()
@@ -88,34 +81,51 @@ describe('utils', () => {
     })
   })
 
-  // describe('useCoingecko', () => {
-  //   it('should fetch PINE value using coingecko', async () => {
-  //     const fakeData = { pine: { usd: '600' } }
-  //     getRequestStub.resolves(fakeData)
+  describe('useCoingecko', () => {
+    it('should fetch PINE value of 3 tokens using coingecko', async () => {
+      const fakeData = { pine: { usd: 0.01201002 } }
+      getRequestStub = async () => fakeData
 
-  //     const { useCoingecko } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
-  //     const resultFunction = await useCoingecko(3)
-  //     const result = await resultFunction()
-  //     const expectedValue = new BigNumber(1800)
+      const { useCoingecko } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
+      const resultFunction = await useCoingecko(3)
+      const result = await resultFunction()
+      const expectedValue = new BigNumber(0.03603006)
 
-  //     expect(result.amount.toString()).to.eql(expectedValue.toString())
-  //     expect(result.currency).to.eql('USD')
-  //   })
+      expect(result.amount.toString()).to.eql(expectedValue.toString())
+      expect(result.currency).to.eql('USD')
+    })
 
-  //   it('should handle errors from coingecko', async () => {
-  //     getRequestStub.rejects(new Error('API Error'))
+    it('should fetch PINE value using coingecko without params', async () => {
+      const fakeData = { pine: { usd: 0.01201002 } }
+      getRequestStub = async () => fakeData
 
-  //     const { useCoingecko } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
+      const { useCoingecko } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
+      const resultFunction = await useCoingecko()
+      const result = await resultFunction()
+      const expectedValue = new BigNumber(0.01201002)
 
-  //     try {
-  //       await useCoingecko(3)
-  //     } catch (err) {
-  //       if (err instanceof CustomError) {
-  //         expect(err.code).to.eql('ERR_GET_PINE_VALUE_USD_USE_COINGECKO')
-  //       } else {
-  //         throw err
-  //       }
-  //     }
-  //   })
-  // })
+      expect(result.amount.toString()).to.eql(expectedValue.toString())
+      expect(result.currency).to.eql('USD')
+    })
+
+    it('should handle errors from coingecko', async () => {
+      getRequestStub = async () => {
+        throw new Error('Network Error')
+      }
+
+      const { useCoingecko } = await esmock('./getPineValueUSD', { './getRequest': getRequestStub })
+
+      try {
+        await useCoingecko(3)
+      }
+      catch (err) {
+        if (err instanceof CustomError) {
+          expect(err.code).to.eql('ERR_GET_PINE_VALUE_USD_USE_COINGECKO')
+        }
+        else {
+          throw err
+        }
+      }
+    })
+  })
 })
